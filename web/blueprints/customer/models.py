0c58c30f17a2acdb63a5814b1b46fb86f21c747c
shirwac62 <c.naafacaxmed962@gmail.com>
2022-03-09 15:41:50 +0300

    updating for svreen shots

--- web/blueprints/customer/models.py
+++ web/blueprints/customer/models.py
@@ -5,7 +5,7 @@ from web.extensions import db
 
 class Customer(UserMixin, db.Model):
     Customer_Id = db.Column(db.Integer, primary_key=True)
-    First_Name = db.Column(db.String(20), unique=True, nullable=False)
+    First_Name = db.Column(db.String(20), unique=False, nullable=False)
     Last_Name = db.Column(db.String(120), unique=False, nullable=False)
     Address = db.Column(db.String(120), unique=False, nullable=False)
     Contact_Number = db.Column(db.Integer, unique=False, nullable=False)
