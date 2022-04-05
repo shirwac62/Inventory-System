4e2bb49d2c3c22aea58c037d5de4e7433e54e0fd
shirwac62 <c.naafacaxmed962@gmail.com>
2022-04-05 11:22:39 +0300

    Making User Adminstarator

--- cmd_db.py
+++ cmd_db.py
@@ -1,12 +1,7 @@
 import click
-from sqlalchemy import func, or_
-from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy_utils import database_exists, create_database
-from wtforms import form
-
 from web.app import create_app
 from web.blueprints.register.models import User
-from web.extensions import db
+from web.extensions import db, bcrypt
 
 app = create_app()
 db.app = app
@@ -24,16 +19,10 @@ def seed_supper():
     :return: User instance
     """
     user = User.query.filter(User.username == "SEED_ADMIN_USER").first()
-    # user = User.find_by_identity(app.config['SEED_ADMIN_EMAIL'])
     if not user:
-        user = User()
-        user.email = app.config['SEED_ADMIN_EMAIL']
-        user.password = User.encrypt_password(app.config['SEED_ADMIN_PASSWORD'])
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        # user.password = app.config['SEED_ADMIN_PASSWORD']
-        # user.id = app.config['SEED_ADMIN_USER']
-        user.username = app.config['SEED_ADMIN_USER']
-        # user.user_type = "superuser"
+        hashed_password = bcrypt.generate_password_hash(app.config['SEED_ADMIN_PASSWORD']).decode('utf-8')
+        user = User(username=app.config['SEED_ADMIN_USER'], email=app.config['SEED_ADMIN_EMAIL'],
+                    password=hashed_password)
         user.is_active = True
         user.save_to_db()
     return
@@ -42,10 +31,6 @@ def seed_supper():
 @click.command()
 def supper():
     return seed_supper()
-
-
-# if __name__ == '__main__':
-#     supper()
 
 
 cli.add_command(supper)
--- config/settings.py
+++ config/settings.py
@@ -5,12 +5,12 @@ LOG_LEVEL = 'DEBUG'
 SECRET_KEY = 'abdinafac'
 
 # SQLAlchemy.
-db_uri = 'postgresql://postgres:ali12.,ali@127.0.0.1/inv2'
+db_uri = 'postgresql://postgres:ali12.,ali@127.0.0.1/inv3'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = db_uri
 
 # User.
-SEED_ADMIN_EMAIL = "admin@admin"
+SEED_ADMIN_EMAIL = ""
 SEED_ADMIN_PASSWORD = "123bb"
 SEED_ADMIN_USER = "adminstrator"
 PERMANENT_SESSION_LIFETIME = timedelta(minutes=120)
