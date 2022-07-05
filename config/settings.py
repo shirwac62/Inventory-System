4ff0037dc2b5583312e61c7feb6b2c6eb00c0693
shirwac62 <c.naafacaxmed962@gmail.com>
2022-07-05 21:22:19 +0300

    my Last update

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,8 @@ LOG_LEVEL = 'DEBUG'
 SECRET_KEY = 'abdinafac'
 
 # SQLAlchemy.
-db_uri = 'postgresql://postgres:ali12.,ali@127.0.0.1/inv3'
+# db_uri = 'postgresql://postgres:970894cC@localhost/my-project-inventory2'
+db_uri = 'postgres://lowbrbnkjlsldj:faf5e08e0699bb6ef2e69f1b1bbfe455bce2e24e7fb0abcbb1f6f0c85bdb25da@ec2-44-195-162-77.compute-1.amazonaws.com:5432/da9vvo2dg8mj7f?sslmode=require'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = db_uri
 
